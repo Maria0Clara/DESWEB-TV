205c53179603ff5e99d0cfd2b970e97c6446e9fc
maria0clara <mariaclaradossantoschaves@gmail.com>
2026-04-30 21:17:18 -0300

    salvar

--- script.js
+++ script.js
@@ -7,36 +7,16 @@ function addToCart(name, price){
     updateCart();
 }
 
-function removeItem(i){
-    total -= cart[i].price;
-    cart.splice(i,1);
-    updateCart();
-}
-
 function updateCart(){
-
-    const count = document.getElementById("cartCount");
-    if(count) count.innerText = cart.length;
 
     const box = document.getElementById("cartBox");
     if(!box) return;
 
     box.innerHTML = "<h3>🛒 Carrinho</h3>";
 
-    if(cart.length === 0){
-        box.innerHTML += "<p>Vazio</p>";
-        return;
-    }
-
-    cart.forEach((item,i)=>{
-        box.innerHTML += `
-        <div style="display:flex;justify-content:space-between;margin:5px 0;">
-        <span>${item.name} - R$ ${item.price}</span>
-        <button onclick="removeItem(${i})">X</button>
-        </div>`;
+    cart.forEach((item)=>{
+        box.innerHTML += `<p>${item.name} - R$ ${item.price}</p>`;
     });
 
     box.innerHTML += `<hr><b>Total: R$ ${total}</b>`;
 }
-
-window.onload = updateCart;
